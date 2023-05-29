9b61f2b09adae21e47201f99e7ad7e37c4e3c9c6
anjaliargon <anjali.argon@gmail.com>
2023-05-29 15:14:57 +0530

    getApi

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,11 +1,29 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
 
-class HomeController extends GetxController {
+import '../../../Model/getDemoModel.dart';
+
+class HomeController extends GetxController  with GetSingleTickerProviderStateMixin {
   //TODO: Implement HomeController
-
+  final List<Tab> myTabs = <Tab>[
+    const Tab(text: 'Every  Day'),
+    const Tab(text: 'Every hour'),
+  ];
+  late TabController controller;
   final count = 0.obs;
+  var UserList = <getDemoModel>[].obs;
+  RxBool hasData = false.obs;
+
   @override
   void onInit() {
+    controller = TabController(vsync: this, length: myTabs.length);
+    controller.addListener(() {
+      print("index${controller.index}");
+    });
+    fetchAlbumData();
     super.onInit();
   }
 
@@ -20,4 +38,29 @@ class HomeController extends GetxController {
   }
 
   void increment() => count.value++;
+
+  Future<void> fetchAlbumData() async {
+    UserList.clear();
+    http.Response response = await http
+        .get(Uri.parse("https://jsonplaceholder.typicode.com/users"))
+        .catchError((error) {
+      print("Error: $error");
+    });
+    print("Data := ${response.statusCode}");
+    if (response.statusCode == 200) {
+      hasData.value = true;
+      print("Data := ${jsonDecode(response.body)}");
+
+      List<dynamic> res = jsonDecode(response.body);
+      res.forEach((element) {
+        UserList.add(getDemoModel.fromJson(element));
+      });
+      print("My Data := ${UserList}");
+    } else {
+      hasData.value = true;
+      Get.snackbar('Error Loding Data!',
+          'Server responded:${response.statusCode}:${response.reasonPhrase
+              .toString()}');
+    }
+  }
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -8,7 +8,7 @@ import '../../../routes/app_pages.dart';
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
-  const HomeView({Key? key}) : super(key: key);
+  HomeView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -34,21 +34,108 @@ class HomeView extends GetView<HomeController> {
       drawer: Drawer(
         child: ListView(
           children: [
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.lightBlue
-              ),
+            const DrawerHeader(
+                decoration: BoxDecoration(
+                    color: Colors.lightBlue
+                ),
                 child: Column(
-              children: [],
-            ))
+                  children: [],
+                ))
           ],
         ),
       ),
-      body: const Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      // body:  Center(
+      //   child: DefaultTabController(
+      //     length: 3,
+      //     child: Column(
+      //       children: [
+      //         const Text(
+      //           'HomeView is working',
+      //           style: TextStyle(fontSize: 20),
+      //         ),
+      //         Container(
+      //             height: 45,
+      //             decoration: BoxDecoration(
+      //                 color: Colors.grey[300],
+      //                 borderRadius: BorderRadius.circular(10.0)),
+      //             child: TabBar(
+      //               controller: controller.controller,
+      //               indicator: BoxDecoration(
+      //                   // color: ColorConstant.blueFE,
+      //                   borderRadius: BorderRadius.circular(10.0)),
+      //               labelColor: Colors.white,
+      //               unselectedLabelColor: Colors.black,
+      //
+      //               tabs:  controller.myTabs,
+      //             )),
+      //
+      //       ],
+      //     ),
+      //   ),
+      // ),
+      body:  Center(
+          child: Column(
+            children: [
+              Expanded(
+                child: ListView.builder(
+                  itemCount: controller.UserList.length,
+                  itemBuilder: (context, i) {
+                    return InkWell(
+                        onTap: () {
+                          // Get.to(Profile(),arguments: {
+                          //   "id":controller.UserList[i].id,
+                          //   "name":controller.UserList[i].name,
+                          //   "email":controller.UserList[i].email,
+                          //   "mobile":controller.UserList[i].mobile,
+                          //   "adr":controller.UserList[i].adr,
+                          //   "gender":controller.UserList[i].gender,
+                          //   "role":controller.UserList[i].role,
+                          //   "salary":controller.UserList[i].salary,
+                          //   "pass":controller.UserList[i].pass,
+                          //   "img":controller.UserList[i].img,
+                          //   "date":controller.UserList[i].date,
+                          //   "joining":controller.UserList[i].joining,
+                          // });
+                        },
+                        child: Container(
+                          margin: EdgeInsets.only(top: 10, right: 10, left: 10),
+                          decoration: BoxDecoration(
+                              color: Colors.grey[300],
+                              border: Border.all(),
+                              borderRadius: BorderRadius.only(
+                                  topRight: Radius.circular(15),
+                                  bottomLeft: Radius.circular(15))),
+                          child: ListTile(
+                            title: Mygetwidget(
+                                tital: "Name",
+                                data: "${controller.UserList[i].name}"),
+                            subtitle: Mygetwidget(
+                                tital: "Email",
+                                data: "${controller.UserList[i].email}"),
+                            trailing: IconButton(
+                              onPressed: () async {
+                                // await controller.deletuser(controller.UserList[i].email);
+                                controller.fetchAlbumData();
+                              },
+                              icon: Icon(Icons.delete),
+                            ),
+                          ),
+                        ));
+                  },
+                ),
+              )
+            ],
+          )
+      ),
+    );
+  }
+
+  Widget Mygetwidget({required String tital, required String data}) {
+    return Container(
+      margin: EdgeInsets.only(left: 10, top: 5),
+      child: Text(
+        "$tital:-$data",
+        style: TextStyle(fontWeight: FontWeight.bold),
       ),
     );
   }
